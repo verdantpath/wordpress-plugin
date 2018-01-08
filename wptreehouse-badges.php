87b6c6ed5950b01a6baa843cd16150253852c2da
Matt <m_dittmann@yahoo.com>
2018-01-07 22:44:41 -0800

    update wptreehouse-plugin.php

--- wptreehouse-badges.php
+++ wptreehouse-badges.php
@@ -11,4 +11,33 @@
  *
 */
 
+/*
+* Add a link to our pluain in the admin menu
+* under 'Settings > Treehouse Badges'
+*
+*/
+
+function wptreehouse_badges_menu() {
+  /*
+   * Use the add_options_page function
+   * add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function )
+   *
+  */
+
+  add_options_page(
+    'Official Treehouse Badges Plugin',
+    'Treehouse Badges',
+    'manage_options',
+    'wptreehouse-badges',
+    'wptreehouse_badges_options_page'
+  );
+
+
+}
+add_action( 'admin_menu', 'wptreehouse_badges_menu' );
+
+function wptreehouse_badges_options_page() {
+
+}
+
 ?>
