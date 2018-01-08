f73e92eb43000f8a5d1044ddc73539a6d49f92a0
Matt <m_dittmann@yahoo.com>
2018-01-07 22:34:57 -0800

    update wptreehouse-plugin.php

---
+++ wptreehouse-badges.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ *	Plugin Name: Official Treehouse Badges Plugin
+ *	Plugin URI: http://wptreehouse.com/wptreehouse-badges-plugin/
+ *	Description: Provides both widgets and shortcodes to help you display your Treehouse profile badges on your website.  The official Treehouse badges plugin.
+ *	Version: 1.0
+ *	Author: Matt Dittmann
+ *	Author URI: http://verdantpath.io
+ *	License: GPL2
+ *
+*/
+
+?>
