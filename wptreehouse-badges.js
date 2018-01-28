4b021abdbf2b857b92fd656390c578105c5a79f4
Matt <m_dittmann@yahoo.com>
2018-01-28 13:59:32 -0800

    update js

--- wptreehouse-badges.js
+++ wptreehouse-badges.js
@@ -6,4 +6,13 @@ jQuery(document).ready(function($){
 		$(this).find('.wptreehouse-badge-info').stop(true, true).fadeOut(200);
 	});
 
+	$.post(ajaxurl, {
+		action: 'wptreehouse_badges_refresh_profile'
+
+	}, function( response ) {
+
+		console.log( 'Ajax complete' );
+
+	});
+
 });
